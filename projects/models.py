aa4ad8892538eca21b96c36b22a0f6fa3ffe0456
djorjani <d.jorjani@gmail.com>
2018-01-27 10:21:40 -0500

    - Built V0.1 for models

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Project(models.Model):
-    name = models.CharField(max_length = 40, helper_text = "project name")
-    description = models.TextField(helper_text="project description")
+
+class Campaign(models.Model):
+    name = models.CharField(max_length = 40)
+    description = models.TextField()
     organization = models.ForeignKey('Organization', on_delete = models.CASCADE)
     FUNDING_STATUS = (
         ('p', 'Pending'),
@@ -11,7 +12,7 @@ class Project(models.Model):
         ('c', 'Closed'),
         ('i', 'Inactive')
     )
-    status = models.CharField(max_length=1, choices=FUNDING_STATUS, blank=True, default='m', help_text='Project Funding Status')
+    status = models.CharField(max_length=1, choices=FUNDING_STATUS, blank=True, default='m', help_text='Campaign Funding Status')
 
 
     def __str__(self):
@@ -23,9 +24,9 @@ class Project(models.Model):
         """
         return reverse('model-detail-view', args=[str(self.id)])
 
-class Organization(model.Model):
-    name = models.CharField(max_length = 40, helper_text = "organization name")
-    description = models.TextField(helper_text="organization description")
+class Organization(models.Model):
+    name = models.CharField(max_length = 40)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
@@ -38,28 +39,6 @@ class Organization(model.Model):
 
 
 import uuid  # Required for unique book instances
-
-
-class Donation(models.Model):
-    """
-    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                          help_text="Unique ID for this particular donation")
-    project = models.ForeignKey('Project', on_delete=models.SET_NULL, null=True)
-    donor = models.ForeignKey('User')
-    date = models.DateField(null=True, blank=True)
-    amount = models.CharField('Amount')
-    currency = models.CharField('Currency')
-
-    class Meta:
-        ordering = ["due_back"]
-
-    def __str__(self):
-        """
-        String for representing the Model object
-        """
-        return '{0} ({1})'.format(self.id, self.book.title)
 
 
 class User(models.Model):
@@ -84,3 +63,21 @@ class User(models.Model):
         String for representing the Model object.
         """
         return '{0}, {1}'.format(self.last_name, self.first_name)
+
+class Donation(models.Model):
+        """
+        Model representing a specific copy of a book (i.e. that can be borrowed from the library).
+        """
+        id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                              help_text="Unique ID for this particular donation")
+        campaign = models.ForeignKey('Campaign', on_delete=models.SET_NULL, null=True)
+        donor = models.ForeignKey('User', on_delete = "Protect")
+        date = models.DateField(null=True, blank=True)
+        amount = models.CharField('Amount', max_length=10)
+        currency = models.CharField('Currency', max_length=3)
+
+        def __str__(self):
+            """
+            String for representing the Model object
+            """
+            return '{0} ({1})'.format(self.id, self.book.title)
