3b10e5b011a9432d2ec93852719e6beb8419d424
djorjani <d.jorjani@gmail.com>
2018-03-04 16:12:29 -0500

    - Added 127.0.0.1 to allowed hosts - added db.sqlite3

--- OpenHand/settings.py
+++ OpenHand/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8qt*s6#jd6g2o@%j8%tiq&^os(o1h8f10g0#8jxue*du_99fuk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".ca-central-1.elasticbeanstalk.com", "localhost"]
+ALLOWED_HOSTS = [".ca-central-1.elasticbeanstalk.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
